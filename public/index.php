7bb8f1ec8a4fa3de5e39438a3d9715712c547d22
Littlecatowo <tobytw3@gmail.com>
2025-11-21 06:54:35 +0800

    Testing log code

--- public/index.php
+++ public/index.php
@@ -19,8 +19,8 @@
     use Monolog\Handler\StreamHandler;
 
     // create a log channel
-    $log = new Logger('name');
-    $log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
+    $log = new Logger('WISD');
+    $log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
 
     // add records to the log
     $log->warning('Foo');
